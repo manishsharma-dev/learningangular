79d0584cf0aa4d0995ae6034bdd3632e6b749ce3
manishsharma-dev <35063672+manishsharma-dev@users.noreply.github.com>
2023-07-30 20:16:45 +0530

    subjects

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, ReplaySubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'learningangular';
+export class AppComponent implements OnInit {
+  newSubject = new Subject<number>();
+  newBehaviourSubject = new BehaviorSubject<string>("");
+  replaySubject = new ReplaySubject(5);
+  subjectValue!: number;
+  behaviourSubjectValue!: string;
+  replaySubjectValue!: string;
+  ngOnInit(): void {
+    // this.assignNewValues();
+    // this.subscribeToSubject();
+    // this.subscribeToBehaviourSubject()
+    this.handleReplaySubject();
+  }
+
+  subscribeToSubject() {
+    this.newSubject.subscribe((value: number) => {
+      console.log("subject Subscribed : " + value);
+      this.subjectValue = value;
+    });
+  }
+
+  setSubjectValue(value: number) {
+    this.newSubject.next(value);
+  }
+
+  subscribeToBehaviourSubject() {
+    this.newBehaviourSubject.subscribe((value: string) => {
+      console.log("Behaviour subject Subscribed : " + value);
+      this.behaviourSubjectValue = value;
+    })
+  }
+
+  setBehaviourValue(value: string) {
+    this.newBehaviourSubject.next(value);
+  }
+
+  setReplaySubject() {
+
+  }
+
+  assignNewValues() {
+    this.setSubjectValue(Math.random() * 10);
+    this.setBehaviourValue("Behave" + Math.random() * 10);
+    this.kepSettingValues();
+  }
+
+  kepSettingValues() {
+    for (var i = 0; i < 10; i++) {
+      this.setSubjectValue((i * 10));
+      this.setBehaviourValue("Behave" + (i * 10));
+    }
+  }
+
+  handleReplaySubject() {
+    for (var i = 0; i < 10; i++) {
+      this.replaySubject.next("Replay" + (i * 10));
+    }
+    this.replaySubject.subscribe((replay) => console.log(replay));
+  }
 }
